78d379708a9980893d1d4f0ef175ebca4e8a560e
AleV1llanueva <163602565+AleV1llanueva@users.noreply.github.com>
2026-03-07 19:12:03 -0600

    Primer commit: Parte de persona 1 terminada

---
+++ ETL/conexion.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+def obtener_conexion():
+    try:
+        server = 'localhost,1433' 
+        database = 'BD_CAFETERIA' 
+        username = 'sa'
+        password = input("Dime tu contraseña para SQL Server: ")
+
+        connection_string = (
+            f'DRIVER={{ODBC Driver 17 for SQL Server}};'
+            f'SERVER={server};'
+            f'DATABASE={database};'
+            f'UID={username};'
+            f'PWD={password};'
+            'Encrypt=no;'
+            'TrustServerCertificate=yes;'
+        )
+        
+        connection_url = f"mssql+pyodbc:///?odbc_connect={connection_string}"
+        engine = create_engine(connection_url)
+        
+        return engine
+        
+    except Exception as e:
+        print(f"Error al conectar: {e}")
+        return None
+
+def extraer_datos():
+    engine = obtener_conexion()
+    if engine:
+        try:
+            query = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'"
+            df = pd.read_sql(query, engine)
+            print("///////////////////////////////////////////////////////////////////")
+            print("SE BIENVENIDO A NUESTRO ETL")
+            print("Porfavor indica el numero de tabla de la cual deseas extraer los datos---------> \n")
+            print(df.head())
+            TABLA = int(input("\nIndica el numero de la tabla que deseas: "))
+        except Exception as e:
+            print(f"Error al extraer los datos de la tabla de origen:{e}")
+        finally:
+            engine.dispose()
+        return df
+    
